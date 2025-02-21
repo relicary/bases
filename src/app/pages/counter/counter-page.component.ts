0d5eacc73b0271a31accd6d05d3f76ff6d678a16
Juan Cremades <relicary@gmail.com>
2025-02-21 13:37:34 +0100

    Added a CounterPage component

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: `
+    <h1>Hi World</h1>
+    <h2>Counter Component Page</h2>
+  `
+})
+export class CounterPageComponent {
+
+}
