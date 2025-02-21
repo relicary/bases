d763c0a30ee46aeadb7149a8bc323e86b60325c3
Juan Cremades <relicary@gmail.com>
2025-02-21 13:46:19 +0100

    Added the HTML and TS code to implement a basic counter

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -2,10 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   template: `
-    <h1>Hi World</h1>
-    <h2>Counter Component Page</h2>
+    <h1>Counter {{ this.counter }}</h1>
+    <button (click)="increaseBy(1)">+1</button>
   `
 })
 export class CounterPageComponent {
+
+  counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
 
 }
