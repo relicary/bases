754c7558d2ab6efcc07bcc8041bb24200e5e49a8
Juan Cremades <relicary@gmail.com>
2025-02-21 14:18:59 +0100

    CounterPage: Signal Zoneless example

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,13 +1,23 @@
-import { Component, signal, WritableSignal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal, WritableSignal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
   styleUrl: './counter-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
 
   counter: number = 10;
   counterSignal: WritableSignal<number> = signal(10);
+
+  constructor() {
+    setInterval(() => {
+      // this.counter+=1;
+      this.counterSignal.update(v => v + 1)
+      console.log('Ping')
+    }, 2000
+  );
+  }
 
   increaseBy(value: number): void {
     this.counter += value;
