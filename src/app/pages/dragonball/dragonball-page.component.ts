810f33e9b1a058d5246aedef79062fc4dba5d4a8
Juan Cremades <relicary@gmail.com>
2025-02-25 17:02:39 +0100

    Dragonball Page: Added the new method to add an element to the list using the fomr

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -13,6 +13,9 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal('Trunks');
+  power = signal(100);
+
   characters: WritableSignal<Character[]> = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9000 },
     {id: 2, name: 'Krilin', power: 2500 },
@@ -27,4 +30,24 @@ export class DragonballPageComponent {
       'text-danger': true,
     }
   })
+
+  addCharacter() {
+    if ( !this.name() || ! this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update( (list) => [...list, newCharacter])
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
