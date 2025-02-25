524a251461b6cb3c8a56c8f4531fe5ae171df3d2
Juan Cremades <relicary@gmail.com>
2025-02-25 16:16:02 +0100

    Added Dragonball Page Component

---
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css'
+})
+export class DragonballPageComponent {
+
+}
