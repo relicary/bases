2ffa3edfa6f63ab7a7afc57c89d93c660b2b73df
Juan Cremades <relicary@gmail.com>
2025-02-25 16:43:45 +0100

    Dragonball Page: Added the new IF clause to avoid render non valid characters

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -15,10 +15,11 @@ export class DragonballPageComponent {
 
   characters: WritableSignal<Character[]> = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9000 },
-    {id: 1, name: 'Krilin', power: 2500 },
-    {id: 1, name: 'Gohan', power: 9500 },
-    {id: 1, name: 'Pikolo', power: 3000 },
-    {id: 1, name: 'Vegeta', power: 8000 }
+    {id: 2, name: 'Krilin', power: 2500 },
+    {id: 3, name: 'Gohan', power: 9500 },
+    {id: 4, name: 'Pikolo', power: 3000 },
+    {id: 5, name: 'Vegeta', power: 8000 },
+    {id: 6, name: 'Yamcha', power: 800 },
   ]);
 
   powerClasses = computed(() => {
